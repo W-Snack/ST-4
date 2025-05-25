05e16cd97cb5205d3ef4390cba26be853f56c7cf
W-Snack <90415228+W-Snack@users.noreply.github.com>
2025-05-26 00:08:03 +0300

    -_-

--- BugPro/Program.cs
+++ BugPro/Program.cs
@@ -1,99 +1,94 @@
 ﻿using Stateless;
 using System;
-
-public class Bug
+namespace BugPro
 {
-    public enum State { Open, Assigned, Defered, Closed, Testing, Resolved, Reopened }
-    private enum Trigger { Assign, Defer, Close, Test, Resolve, Reopen }
-
-    private StateMachine<State, Trigger> sm;
-
-    public Bug(State state)
+    public class Bug
     {
-        sm = new StateMachine<State, Trigger>(state);
+        public enum State { Open, Assigned, Defered, Closed, Testing, Resolved, Reopened, Verified, Rejected }
+        private enum Trigger { Assign, Defer, Close, Test, Resolve, Reopen, Verify, Reject }
 
-        sm.Configure(State.Open)
-            .Permit(Trigger.Assign, State.Assigned);
+        private StateMachine<State, Trigger> sm;
 
-        sm.Configure(State.Assigned)
-            .Permit(Trigger.Close, State.Closed)
-            .Permit(Trigger.Defer, State.Defered)
-            .Permit(Trigger.Test, State.Testing)
-            .Ignore(Trigger.Assign);
+        public Bug(State state)
+        {
+            sm = new StateMachine<State, Trigger>(state);
 
-        sm.Configure(State.Defered)
-            .Permit(Trigger.Assign, State.Assigned);
+            ConfigureTransitions();
+        }
 
-        sm.Configure(State.Closed)
-            .Permit(Trigger.Reopen, State.Reopened);
+        private void ConfigureTransitions()
+        {
+            sm.Configure(State.Open)
+                .Permit(Trigger.Assign, State.Assigned)
+                .PermitReentry(Trigger.Defer);
 
-        sm.Configure(State.Testing)
-            .Permit(Trigger.Resolve, State.Resolved)
-            .Permit(Trigger.Assign, State.Assigned);
+            sm.Configure(State.Assigned)
+                .Permit(Trigger.Close, State.Closed)
+                .Permit(Trigger.Defer, State.Defered)
+                .Permit(Trigger.Test, State.Testing)
+                .Permit(Trigger.Reject, State.Rejected)
+                .Ignore(Trigger.Assign);
 
-        sm.Configure(State.Resolved)
-            .Permit(Trigger.Close, State.Closed)
-            .Permit(Trigger.Reopen, State.Reopened);
+            sm.Configure(State.Defered)
+                .Permit(Trigger.Assign, State.Assigned);
 
-        sm.Configure(State.Reopened)
-            .Permit(Trigger.Assign, State.Assigned);
+            sm.Configure(State.Closed)
+                .Permit(Trigger.Reopen, State.Reopened);
+
+            sm.Configure(State.Testing)
+                .Permit(Trigger.Resolve, State.Resolved)
+                .Permit(Trigger.Assign, State.Assigned)
+                .Permit(Trigger.Reject, State.Rejected);
+
+            sm.Configure(State.Resolved)
+                .Permit(Trigger.Close, State.Closed)
+                .Permit(Trigger.Reopen, State.Reopened)
+                .Permit(Trigger.Verify, State.Verified);
+
+            sm.Configure(State.Reopened)
+                .Permit(Trigger.Assign, State.Assigned);
+
+            sm.Configure(State.Verified)
+                .Permit(Trigger.Close, State.Closed)
+                .Permit(Trigger.Reopen, State.Reopened);
+
+            sm.Configure(State.Rejected)
+                .Permit(Trigger.Assign, State.Assigned)
+                .Permit(Trigger.Defer, State.Defered);
+        }
+
+        public void Close() => FireWithLog(Trigger.Close, "Close");
+        public void Assign() => FireWithLog(Trigger.Assign, "Assign");
+        public void Defer() => FireWithLog(Trigger.Defer, "Defer");
+        public void Test() => FireWithLog(Trigger.Test, "Test");
+        public void Resolve() => FireWithLog(Trigger.Resolve, "Resolve");
+        public void Reopen() => FireWithLog(Trigger.Reopen, "Reopen");
+        public void Verify() => FireWithLog(Trigger.Verify, "Verify");
+        public void Reject() => FireWithLog(Trigger.Reject, "Reject");
+
+        private void FireWithLog(Trigger trigger, string action)
+        {
+            sm.Fire(trigger);
+            Console.WriteLine(action);
+        }
+
+        public State GetState() => sm.State;
     }
 
-    public void Close()
+    public class Program
     {
-        sm.Fire(Trigger.Close);
-        Console.WriteLine("Close");
-    }
-
-    public void Assign()
-    {
-        sm.Fire(Trigger.Assign);
-        Console.WriteLine("Assign");
-    }
-
-    public void Defer()
-    {
-        sm.Fire(Trigger.Defer);
-        Console.WriteLine("Defer");
-    }
-
-    public void Test()
-    {
-        sm.Fire(Trigger.Test);
-        Console.WriteLine("Test");
-    }
-
-    public void Resolve()
-    {
-        sm.Fire(Trigger.Resolve);
-        Console.WriteLine("Resolve");
-    }
-
-    public void Reopen()
-    {
-        sm.Fire(Trigger.Reopen);
-        Console.WriteLine("Reopen");
-    }
-
-    public State GetState()
-    {
-        return sm.State;
-    }
-}
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        var bug = new Bug(Bug.State.Open);
-        bug.Assign();
-        bug.Test();
-        bug.Resolve();
-        bug.Close();
-        bug.Reopen();
-        bug.Assign();
-        bug.Defer();
-        bug.Assign();
-        Console.WriteLine(bug.GetState());
+        public static void Main(string[] args)
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Test();
+            bug.Resolve();
+            bug.Close();
+            bug.Reopen();
+            bug.Assign();
+            bug.Defer();
+            bug.Assign();
+            Console.WriteLine(bug.GetState());
+        }
     }
 }
--- BugTests/UnitTest1.cs
+++ BugTests/UnitTest1.cs
@@ -1,11 +1,220 @@
-﻿namespace BugTests
+﻿using BugPro;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BugTests
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InitialState_ShouldBeOpen()
         {
+            var bug = new Bug(Bug.State.Open);
+            Assert.AreEqual(Bug.State.Open, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Assign_FromOpen_ShouldTransitionToAssigned()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Close_FromOpen_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Close();
+        }
+
+        [TestMethod]
+        public void Test_FromAssigned_ShouldTransitionToTesting()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Test();
+            Assert.AreEqual(Bug.State.Testing, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Resolve_FromTesting_ShouldTransitionToResolved()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Test();
+            bug.Resolve();
+            Assert.AreEqual(Bug.State.Resolved, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Verify_FromResolved_ShouldTransitionToVerified()
+        {
+            var bug = new Bug(Bug.State.Open);
+            ExecuteFlow(bug, Bug.State.Resolved);
+            bug.Verify();
+            Assert.AreEqual(Bug.State.Verified, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Reopen_FromClosed_ShouldTransitionToReopened()
+        {
+            var bug = new Bug(Bug.State.Open);
+            ExecuteFlow(bug, Bug.State.Closed);
+            bug.Reopen();
+            Assert.AreEqual(Bug.State.Reopened, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Resolve_FromAssigned_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Resolve();
+        }
+
+        [TestMethod]
+        public void DoubleAssign_FromAssigned_ShouldKeepState()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            var initialState = bug.GetState();
+            bug.Assign();
+            Assert.AreEqual(initialState, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Reject_FromAssigned_ShouldTransitionToRejected()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Reject();
+            Assert.AreEqual(Bug.State.Rejected, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Assign_FromRejected_ShouldTransitionToAssigned()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Reject();
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+        }
+
+        [TestMethod]
+        public void FullLifecycle_ShouldReachClosedState()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Test();
+            bug.Resolve();
+            bug.Verify();
+            bug.Close();
+            Assert.AreEqual(Bug.State.Closed, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Verify_FromClosed_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Closed);
+            bug.Verify();
+        }
+
+        [TestMethod]
+        public void Reopen_FromVerified_ShouldTransitionToReopened()
+        {
+            var bug = new Bug(Bug.State.Open);
+            ExecuteFlow(bug, Bug.State.Verified);
+            bug.Reopen();
+            Assert.AreEqual(Bug.State.Reopened, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Defer_FromRejected_ShouldTransitionToDefered()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Reject();
+            bug.Defer();
+            Assert.AreEqual(Bug.State.Defered, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Test_FromDefered_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Defered);
+            bug.Test();
+        }
+
+        [TestMethod]
+        public void Close_FromVerified_ShouldTransitionToClosed()
+        {
+            var bug = new Bug(Bug.State.Open);
+            ExecuteFlow(bug, Bug.State.Verified);
+            bug.Close();
+            Assert.AreEqual(Bug.State.Closed, bug.GetState());
+        }
+
+        [TestMethod]
+        public void ComplexFlow_WithReopens_ShouldReachExpectedState()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Test();
+            bug.Resolve();
+            bug.Verify();
+            bug.Close();
+            bug.Reopen();
+            bug.Assign();
+            bug.Defer();
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InvalidTransition_FromResolvedToTesting_ShouldThrow()
+        {
+            var bug = new Bug(Bug.State.Resolved);
+            bug.Test();
+        }
+
+        [TestMethod]
+        public void MultipleRejects_ShouldFollowCorrectPath()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Reject();
+            bug.Assign();
+            bug.Reject();
+            bug.Defer();
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+        }
+
+        private void ExecuteFlow(Bug bug, Bug.State targetState)
+        {
+            switch (targetState)
+            {
+                case Bug.State.Closed:
+                    bug.Assign();
+                    bug.Test();
+                    bug.Resolve();
+                    bug.Close();
+                    break;
+                case Bug.State.Verified:
+                    bug.Assign();
+                    bug.Test();
+                    bug.Resolve();
+                    bug.Verify();
+                    break;
+            }
         }
     }
 }
